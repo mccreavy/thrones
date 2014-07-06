8796aed41f0db8967ac9a2e21b6b63c7afce4939
mccreavy <mike@mccreavy.com>
2014-07-06 02:43:49 -0700

    render full nesting of selected menu item

--- closure-thrones/thrones/ui/MenuButtonRenderer.js
+++ closure-thrones/thrones/ui/MenuButtonRenderer.js
@@ -7,3 +7,26 @@ thrones.ui.MenuButtonRenderer = function() {
 };
 goog.inherits(thrones.ui.MenuButtonRenderer, goog.ui.MenuButtonRenderer);
 goog.addSingletonGetter(thrones.ui.MenuButtonRenderer);
+
+/**
+ * Takes the button's root element and returns the parent element of the
+ * button's contents.  Overrides the superclass implementation by taking
+ * the nested DIV structure of menu buttons into account.
+ * @param {Element} element Root element of the button whose content element
+ *     is to be returned.
+ * @return {Element} The button's content element.
+ * @override
+ */
+thrones.ui.MenuButtonRenderer.prototype.getContentElement = function(element) {
+  var x = thrones.ui.MenuButtonRenderer.superClass_.getContentElement.call(this,
+      /** @type {Element} */ (element && element.firstChild));
+  
+  console.log("x is ", x , goog.typeOf(x));
+  if (null == x) return x;
+  var x = document.createElement('div');
+  x.appendChild(document.createTextNode('Hey Monkey'));
+  x = document.createTextNode("BOOO");
+  console.log("returning monkey ", x, goog.typeOf(x));
+  return x;
+  
+};
--- closure-tutorials/select-custom-skin/select.js
+++ closure-tutorials/select-custom-skin/select.js
@@ -4,18 +4,29 @@ goog.require('goog.ui.Option');
 goog.require('goog.ui.Select');
 goog.require('goog.ui.MenuItem');
 goog.require('goog.ui.SubMenu');
+goog.require('thrones.ui.Select');
 goog.require('thrones.ui.MenuButtonRenderer');
+goog.require('thrones.ui.MenuRenderer');
 
 select.start = function() {
+  // This is the button that launches the menu ("This is the caption...")
   var menuButtonRenderer = new thrones.ui.MenuButtonRenderer();
 
-  var select = new goog.ui.Select("This is the Caption", null, menuButtonRenderer);
+  // This is the actual "menu" (Alice, Barbie, ...)
+  var menuRenderer = new thrones.ui.MenuRenderer();
+
+  var select = new thrones.ui.Select("This is the Caption"); //, null,
+//    menuButtonRenderer, null, menuRenderer);
   select.addItem(new goog.ui.MenuItem('Alice'));
   select.addItem(new goog.ui.MenuItem('Barbie'));
   select.addItem(new goog.ui.MenuItem('Chrissie'));
   var subMenu = new goog.ui.SubMenu('More');
   subMenu.addItem(new goog.ui.MenuItem('Deeper'));
   select.addItem(subMenu);
+  var subMenu2 = new goog.ui.SubMenu('Even');
+  subMenu2.addItem(new goog.ui.MenuItem('Deepest'));
+  subMenu.addItem(subMenu2);
+
   //select.setSelectedIndex(1);
 
   // render takes an optional parent element, if not it goes to document body.
